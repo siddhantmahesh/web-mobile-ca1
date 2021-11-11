d2288274993fa8d1aa754ce76275bc135d2502d5
siddhantmahesh <92044233+siddhantmahesh@users.noreply.github.com>
2021-11-11 13:13:45 +0000

    card layout for movies

---
+++ data.js
@@ -0,0 +1,49 @@
+const data = {
+  movies: [
+    {
+      id: '0',
+      title: 'sampleMovie0',
+      desc: 'generic movie desc',
+      releaseDate: '1945-10-01',
+      director: 'directorName',
+      producer: 'productionName',
+      rating: '7.5',
+      actors: 'michael j. scott,alison tyler,ben spies',
+      genre: 'action,drama',
+      coverPic: './movies/0/cover.jpg',
+      images:
+        './movies/0/1.jpg,./movies/0/2.jpg,./movies/0/3.jpg,./movies/0/4.jpg',
+      price: '$7.99',
+    },
+    {
+      id: '1',
+      title: 'sampleMovie1',
+      desc: 'generic movie desc1',
+      releaseDate: '1998-10-01',
+      director: 'directorName',
+      producer: 'productionName2',
+      rating: '7',
+      actors: 'justin timberlake,ann hathway',
+      genre: 'romance',
+      coverPic: './movies/1/cover.jpg',
+      images:
+        './movies/1/1.jpg,./movies/1/2.jpg,./movies/1/3.jpg,./movies/1/4.jpg',
+      price: '$5.99',
+    },
+    {
+      id: '2',
+      title: 'sampleMovie2',
+      desc: 'generic movie desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: 'shahrukh khan,salman khan',
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+  ],
+}
