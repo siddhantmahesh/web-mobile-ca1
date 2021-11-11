79edeb2ffa74946ee8ea549aa2e36a243d2c815f
NickSarode <93203828+NickSarode@users.noreply.github.com>
2021-11-11 19:19:56 +0000

    website and contact page
    First website, contact page created

---
+++ index.js
@@ -0,0 +1,16 @@
+function movieSection(){
+  document.getElementById('sec-2').style.display = 'none';
+  document.getElementById('sec-3').style.display = 'none';
+  document.getElementById('sec-4').style.display = 'none';
+}
+
+function genreclick(){
+  document.getElementById('sec-2').style.display = "block";
+  document.getElementById('sec-2').style.display = "flex";
+
+}
+
+function contactUsClick(){
+  document.getElementById('sec-4').style.display = 'block';
+  //document.getElementById('sec-4').style.display = 'flex';
+}
