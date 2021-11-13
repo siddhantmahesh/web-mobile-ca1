1722d2062dea625301571766528bb6bdae141306
siddhantmahesh <92044233+siddhantmahesh@users.noreply.github.com>
2021-11-13 19:42:18 +0000

    updated movie page to render data dynamically

--- data.js
+++ data.js
@@ -3,12 +3,20 @@ const data = {
     {
       id: '0',
       title: 'sampleMovie0',
-      desc: 'generic movie desc',
+      desc: 'movie description',
       releaseDate: '1945-10-01',
       director: 'directorName',
       producer: 'productionName',
       rating: '7.5',
-      actors: 'michael j. scott,alison tyler,ben spies',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
       genre: 'action,drama',
       coverPic: './movies/0/cover.jpg',
       images:
@@ -18,12 +26,20 @@ const data = {
     {
       id: '1',
       title: 'sampleMovie1',
-      desc: 'generic movie desc1',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc1',
       releaseDate: '1998-10-01',
       director: 'directorName',
       producer: 'productionName2',
       rating: '7',
-      actors: 'justin timberlake,ann hathway',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
       genre: 'romance',
       coverPic: './movies/1/cover.jpg',
       images:
@@ -33,12 +49,296 @@ const data = {
     {
       id: '2',
       title: 'sampleMovie2',
-      desc: 'generic movie desc2',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
       releaseDate: '1995-05-01',
       director: 'directorName5',
       producer: 'productionName3',
       rating: '9.8',
-      actors: 'shahrukh khan,salman khan',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '3',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlank            dnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '4',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '5',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '6',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '7',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '8',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '9',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '10',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '11',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '12',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '13',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
+      genre: 'romance',
+      coverPic: './movies/2/cover.jpg',
+      images:
+        './movies/2/1.jpg,./movies/2/2.jpg,./movies/2/3.jpg,./movies/2/4.jpg',
+      price: '$0.099',
+    },
+    {
+      id: '14',
+      title: 'sampleMovie3',
+      desc: 'dawhd wd lwkdn kanwdk anwkd nkawnd knawkdnn wankld knwad nwlankdnnwakdnwkan dkanlw ndknadn danwdk awndkanwdnalwnd akwn dnawkdnalwndlkandnak wdn adakwnd kawn dlkawnd kanwd njwahdanawjbjvkbabvfgpwh ag wi waiwhdpwdow awdjawdpwa dajd wadajwdjawp dapwodjajdwihafhaowhdahd owhdiado wadawdiahd dhoaiwhdiahwdihawda diahdhawdhwhdoawhd hawidhoahwdhawodha doahwdahwdho waodhaowdao dawhdwahdawihdahwodh awhdiawhdhwa wadhaohwdhawdoawhda dwadaihwdhawd doawhdhawdhahwdiahdhawhd hwada whdawhdawhdoawhdah dwahdhawhdapwodjajddhawhd odwhai hdiahdhawhdhwuafawjdajw aw wahdhawhiwadwhadoiohwa fahwf fwaihfahwfhaawdoiahwdaihdawgwadajdahawfjawrihhgwafjg9 ua uwaud j9wa99wa4aw 90udahidw09dn desc2',
+      releaseDate: '1995-05-01',
+      director: 'directorName5',
+      producer: 'productionName3',
+      rating: '9.8',
+      actors: [
+        {
+          actorName: 'name',
+          actorImg: './movies/actors/0.jpg'
+        },
+        {
+          actorName: 'name1',
+          actorImg: './movies/actors/1.jpg'
+        }],
       genre: 'romance',
       coverPic: './movies/2/cover.jpg',
       images:
